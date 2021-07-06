84c0505c09bb4b24c3d5a86bd0d55b0360490269
John Gillan <jgillan@protonmail.com>
2021-07-06 15:56:29 +0100

    fixed redirect on page load

--- src/components/routing/PrivateRoute.js
+++ src/components/routing/PrivateRoute.js
@@ -4,28 +4,24 @@ import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 
 const PrivateRoute = ({
-    component: Component,
-    auth: { isAuthenticated, loading },
-    ...rest
+  component: Component,
+  auth: { isAuthenticated, loading },
+  ...rest
 }) => (
-    <Route
-        {...rest}
-        render={props =>
-            !isAuthenticated && !loading ? (
-                <Redirect to="/login" />
-            ) : (
-                <Component {...props} />
-            )
-        }
-    />
+  <Route
+    {...rest}
+    render={props =>
+      !isAuthenticated ? <Redirect to="/login" /> : <Component {...props} />
+    }
+  />
 );
 
 PrivateRoute.propTypes = {
-    auth: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
-    auth: state.auth,
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps)(PrivateRoute);
