18bed8f66bdb9790982fd8847d1bf58a9cbe6f91
John Gillan <jgillan@protonmail.com>
2021-07-21 23:42:30 +0100

    added types to dependencies

--- src/components/Watchlist.tsx
+++ src/components/Watchlist.tsx
@@ -22,59 +22,61 @@ const Watchlist = () => {
   return (
     <main id="watchlist-page" className="narrow-container">
       <h1 className="watchlist-heading">Watchlist</h1>
-      <div className="watchlist-button-container">
-        <div className="watchlist-number-of-films-container">
-          {films.length} films
-        </div>
-        <div>
-          <button
-            className={classNames(
-              { 'watchlist-button-pressed': watchlist.sortedByTitle },
-              'watchlist-button'
-            )}
-            name="title"
-            onClick={sortByTitle}
-          >
-            Sort by title
-          </button>
-          <button
-            className={classNames(
-              { 'watchlist-button-pressed': watchlist.sortedByYear },
-              'watchlist-button'
-            )}
-            name="year"
-            onClick={sortByYear}
-          >
-            Sort by year
-          </button>
-          <button
-            className={classNames(
-              { 'watchlist-button-pressed': watchlist.sortedByDuration },
-              'watchlist-button'
-            )}
-            name="year"
-            onClick={sortByDuration}
-          >
-            Sort by duration
-          </button>
-        </div>
-      </div>
       {watchlist.loading ? (
         <Spinner />
       ) : films.length > 0 ? (
-        <ul className="watchlist">
-          {films.map((film: any) => (
-            <WatchlistFilm
-              _id={film._id}
-              overview={film.overview}
-              title={film.title}
-              year={film.year}
-              poster_path={film.poster_path}
-              runtime={film.runtime}
-              key={film._id}
-            />
-          ))}
-        </ul>
+        <>
+          <div className="watchlist-button-container">
+            <div className="watchlist-number-of-films-container">
+              {films.length} films
+            </div>
+            <div>
+              <button
+                className={classNames(
+                  { 'watchlist-button-pressed': watchlist.sortedByTitle },
+                  'watchlist-button'
+                )}
+                name="title"
+                onClick={sortByTitle}
+              >
+                Sort by title
+              </button>
+              <button
+                className={classNames(
+                  { 'watchlist-button-pressed': watchlist.sortedByYear },
+                  'watchlist-button'
+                )}
+                name="year"
+                onClick={sortByYear}
+              >
+                Sort by year
+              </button>
+              <button
+                className={classNames(
+                  { 'watchlist-button-pressed': watchlist.sortedByDuration },
+                  'watchlist-button'
+                )}
+                name="year"
+                onClick={sortByDuration}
+              >
+                Sort by duration
+              </button>
+            </div>
+          </div>
+          <ul className="watchlist">
+            {films.map((film: any) => (
+              <WatchlistFilm
+                _id={film._id}
+                overview={film.overview}
+                title={film.title}
+                year={film.year}
+                poster_path={film.poster_path}
+                runtime={film.runtime}
+                key={film._id}
+              />
+            ))}
+          </ul>
+        </>
       ) : (
         <div className="watchlist-empty-container">
           <h3 className="watchlist-emtpy-text">
