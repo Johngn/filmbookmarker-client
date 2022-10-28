fca8bf974b260b910e8cfb5095dbdc3e16d1d013
John Gillan <jgillan@protonmail.com>
2022-10-28 22:28:20 +0100

    *

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,5 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
-  app.use(
-    "/api",
-    createProxyMiddleware({
-      target: process.env.REACT_APP_PROXY,
-      changeOrigin: true,
-      onProxyRes: response => {
-        response.headers["access-control-allow-origin"] = "*";
-      },
-    })
-  );
+  app.use("/api", createProxyMiddleware({}));
 };
