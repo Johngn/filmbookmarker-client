b2618b97735cbe9b4cae4a6c5dfbaac02abd7df7
John Gillan <jgillan@protonmail.com>
2022-10-28 21:40:55 +0100

    add toml file

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "https://filmmarker-backend.up.railway.app/",
-      // target: process.env.REACT_APP_PROXY,
+      target: process.env.REACT_APP_PROXY,
       changeOrigin: true,
       onProxyRes: response => {
         response.headers["access-control-allow-origin"] = "*";
