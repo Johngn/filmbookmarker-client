e40ca65f241da3df9424e1a10b782951eadf7853
John Gillan <jgillan@protonmail.com>
2022-10-29 11:24:03 +0100

    undo proxy changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,14 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use("/api", createProxyMiddleware({}));
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: process.env.REACT_APP_PROXY,
+      changeOrigin: true,
+      onProxyRes: response => {
+        response.headers['access-control-allow-origin'] = '*';
+      },
+    })
+  );
 };
